c78e343725b513f045da42f87c039d62ea1de53d
Brett Winterstrom <brettwinterstrom@yahoo.com>
2021-02-03 19:53:23 -0800

    new and improved portfolio

---
+++ script.js
@@ -0,0 +1,16 @@
+const linkedIn = document.getElementById("linkedIn");
+const gitHub = document.getElementById("gitHub")
+
+linkedIn.onclick = () => {
+    window.location.href = "https://www.linkedin.com/in/brettwinterstrom/";
+}
+
+gitHub.onclick = () => {
+    window.location.href = "https://github.com/Brettwinterstrom";
+}
+
+email.onclick = () => {
+    window.location.href = "mailto:brettwinterstrom@gmail.com";
+}
+
+
